61ee9ca15dafbbb55166fe2c53e1f0d8f6c7f0d7
oswaldo  martinez <oswaldomartinez@MacBook-Pro-de-oswaldo.local>
2018-11-21 11:32:01 -0600

    partials

--- app.js
+++ app.js
@@ -9,37 +9,47 @@ app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'hbs')
 
+//adding partials
+hbs.registerPartials(__dirname + '/views/partials')
+//helpers 
+// hbs.registerHelper('colores',function(x){
+//     if(x>25) return '<h2 style="color:red">'+x+'</h2>'
+//     else return '<h2 style="color :green">'+x+'</h2>'
+// })
+app.locals.title = 'perro'
 
 
-app.get('/', (req, res)=>{
+
+app.get('/', (req, res)=>{    
     const user = {
         name:'Oswaldinho',
         age:22,
         hobby:'Comer all day long',
         hungry:false        
-    }    
+    }   
+    app.locals.title = user.name 
     res.render('index',user)
 })
 
 app.get('/users',(req, res)=>{
     const tas = 'Los brgas'
     const users = [
-        // {
-        //     name:'Oswaldinho',
-        //     age:22,
-        //     hobby:'Comer all day long',
-        //     hungry:true        
-        // },{
-        //     name:'Dieguinho',
-        //     age:22,
-        //     hobby:'Inyectarse',
-        //     hungry:true        
-        // },{
-        //     name:'Joss',
-        //     age:22,
-        //     hobby:'Play LoL',
-        //     hungry:false        
-        // }   
+        {
+            name:'Oswaldinho',
+            age:22,
+            hobby:'Comer all day long',
+            hungry:true        
+        },{
+            name:'Dieguinho',
+            age:22,
+            hobby:'Inyectarse',
+            hungry:true        
+        },{
+            name:'Joss',
+            age:22,
+            hobby:'Play LoL',
+            hungry:false        
+        }   
     ]
     res.render('users', {users,tas})
 })
